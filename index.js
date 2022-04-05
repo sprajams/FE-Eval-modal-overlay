db71c0cb10eca6faf72030a1d8f44e6602002eab
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-04-05 12:17:54 -0700

    update to include 2 offers and mobile capable

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let showButton = document.querySelector(".showButton");
-showButton.addEventListener("click", showOffer);
+let showButton = document.querySelectorAll(".showButton");
+showButton.forEach((x) => x.addEventListener("click", showOffer));
 
-let offerContainer = document.querySelector(".offerContainer");
-
-let offerAccepted = document.querySelector(".offerAccepted");
+let modal = document.querySelector(".modal");
+modal.addEventListener("keyup", keyup);
 
 let exitButton = document.querySelector(".exitButton");
 exitButton.addEventListener("click", exitOffer);
@@ -11,29 +10,47 @@ exitButton.addEventListener("click", exitOffer);
 let acceptButton = document.querySelector(".acceptButton");
 acceptButton.addEventListener("click", acceptOffer);
 
+let acceptContainer = document.querySelector(".acceptContainer");
+
+let offerContainer = document.querySelector(".offerContainer");
+
+let retryButton = document.querySelector(".retryButton");
+retryButton.addEventListener("click", retryOffer);
+
 function showOffer() {
-  console.log("show me the offer");
   document.body.style.background = "rgba(128, 128, 128, 0.5)";
-  offerContainer.style.display = "block";
+  console.log("modal clicked");
+  modal.style.display = "block";
 }
 
 function exitOffer() {
-  console.log("exit the offer now");
-  offerContainer.style.display = "none";
+  modal.style.display = "none";
   backgroundReset();
 }
 
 function acceptOffer() {
-  console.log("accept the offerr pls");
   accepted();
   backgroundReset();
 }
 
 function accepted() {
+  modal.style.display = "none";
+  acceptContainer.style.display = "block";
   offerContainer.style.display = "none";
-  offerAccepted.style.display = "block";
-  showButton.style.display = "none";
 }
+
 function backgroundReset() {
   document.body.style.background = "white";
+}
+// accessibility, using esc key to exit out of offer pop up
+function keyup(e) {
+  if (e.key === "Escape") {
+    console.log("escaaaaape");
+    exitOffer();
+  }
+}
+
+function retryOffer() {
+  acceptContainer.style.display = "none";
+  offerContainer.style.display = "block";
 }
